7f717b0b05b4195e3af67f157d6dec781ba3f66d
Sayantan Saha <sayantan.somu@gmail.com>
2024-05-01 13:28:41 +0530

    ssk 01.05.2024

--- app/Http/Controllers/IFADashboardController.php
+++ app/Http/Controllers/IFADashboardController.php
@@ -12,27 +12,36 @@ use Illuminate\Support\Facades\Log;
 class IFADashboardController extends Controller
 {
     //
-    function get(Request $request){
+    function get(Request $request)
+    {
         $user = Auth::user();
-        $request->session()->put('month', date('m'));
-        $request->session()->put('year', date('Y'));
-        return view('ifa-dashboard', ['user'=>$user]);
+        if ($user->username == 'iiau'){
+            $request->session()->put('month', date('m'));
+            $request->session()->put('year', date('Y'));
+            return view('ifa-dashboard', ['user' => $user]);
+        }
+        else
+            return redirect()->intended('dashboard');
     }
 
     function find(Request $request){
         $user = Auth::user();
-        $monthYear = $request->get('report_month');
-        Log::debug('Report month is:{report_month}',['report_month'=>$monthYear]);
-        $inputArr = explode('-',$monthYear);
-        $month = $inputArr[1];
-        $year = $inputArr[0];
-        $data = [];
-        $request->session()->put('month', $month);
-        $request->session()->put('year', $year);
-        $sections = Section::with(['reports'=>function($query) use ($month,$year){
-            $query->where('month',$month)->where('year',$year);
-        }])->orderBy('section_name')->get();
+        if(Auth::user()->username=='iiau') {
+            $monthYear = $request->get('report_month');
+            Log::debug('Report month is:{report_month}', ['report_month' => $monthYear]);
+            $inputArr = explode('-', $monthYear);
+            $month = $inputArr[1];
+            $year = $inputArr[0];
+            $data = [];
+            $request->session()->put('month', $month);
+            $request->session()->put('year', $year);
+            $sections = Section::with(['reports' => function ($query) use ($month, $year) {
+                $query->where('month', $month)->where('year', $year);
+            }])->orderBy('section_name')->get();
 
-        return view('ifa-dashboard', ['user'=>$user,'data'=>$sections]);
+            return view('ifa-dashboard', ['user' => $user, 'data' => $sections]);
+        }
+        else
+            return redirect()->intended('dashboard');
     }
 }
